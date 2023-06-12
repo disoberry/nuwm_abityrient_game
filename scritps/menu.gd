eff275c90b6a1a41d4833acd2a6cf230a2527e45
Disobey <korobenko_ak21@nuwm.edu.ua>
2023-06-13 00:42:29 +0300

    music update

--- scritps/menu.gd
+++ scritps/menu.gd
@@ -12,7 +12,7 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/piece_of_paper.tscn")	
 
 func _on_extras_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/extras_menu.tscn")	
 
 
 func _on_exit_button_pressed():
