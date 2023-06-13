791838ee865de4a8d9ff5eefab9c8b4e6e055ef2
Disobey <korobenko_ak21@nuwm.edu.ua>
2023-06-13 23:44:59 +0300

    npc fix

--- scritps/IsDR.gd
+++ scritps/IsDR.gd
@@ -1,6 +1,9 @@
 extends Node
 
+
+var question2was = "false"
 var has_met_v103 = "false"
 var has_found_hint1 = "false"
+var was_in_102 = "false"
 var is_dialogue_running = "false"
 # Called when the node enters the scene tree for the first time.
